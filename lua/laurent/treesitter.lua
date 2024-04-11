57d68e89b4cbf6c3b8da6aed0d1749c782a422ba
lg-epitech <laurent.gonzalez@epitech.eu>
2024-04-11 18:45:23 +0200

    [FIX] treesitter

--- lua/laurent/treesitter.lua
+++ lua/laurent/treesitter.lua
@@ -1,25 +1,28 @@
 local M = {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-          -- A list of parser names, or "all" (the five listed parsers should always be installed)
-          ensure_installed = {"all"},
+    opts = {},
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            -- A list of parser names, or "all" (the five listed parsers should always be installed)
+            ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "python", "bash", "rust", "yaml", "json", "cpp", "c_sharp"},
 
-          -- Install parsers synchronously (only applied to `ensure_installed`)
-          sync_install = false,
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = true,
 
-          -- Automatically install missing parsers when entering buffer
-          -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-          auto_install = true,
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
 
-          -- List of parsers to ignore installing (or "all")
+            -- List of parsers to ignore installing (or "all")
 
-          ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-          -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-          highlight = {
+            highlight = {
                 enable = true,
-        },
-    }
+            },
+        }
+    end
 }
 
 return M
