de26fb61d0b19d6d0a5a3113a06a04af9636bc18
lg-epitech <laurent.gonzalez@epitech.eu>
2024-08-22 11:24:54 +0200

    [FIX] ft specific format

--- after/ftplugin/css.lua
+++ after/ftplugin/css.lua
@@ -1,2 +1,4 @@
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+
+vim.keymap.set("n", "<leader>s", "<Cmd>%!prettier \"%\"<CR>", { noremap=true, silent=true })
--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,9 +1,6 @@
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-
 vim.keymap.set(
     "n",
     "<leader>s",
-    '<Cmd>%!prettier "%"<CR>',
+    ":%!autopep8 %",
     { noremap = true, silent = true }
 )
