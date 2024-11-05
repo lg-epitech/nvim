9f4e4b2939e770f4129ce16af6494fff23e07d2b
lg-epitech <laurent.gonzalez@epitech.eu>
2024-11-05 21:44:01 +0100

    [REM] boo

--- lua/laurent/boo.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-    "LukasPietzschmann/boo.nvim",
-    opts = {},
-    config = function()
-        -- vim.keymap.set("n", "<C-k>", ":lua require('boo').boo()<CR>", {silent = true})
-    end
-}
-
-return M
