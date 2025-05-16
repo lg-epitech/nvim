d621cd61727cb75830a42945f8cabf4e59269d9a
lg-epitech <laurent.gonzalez@epitech.eu>
2025-05-16 19:45:32 +0200

    feat(telescope): include hidden and git files

--- lua/laurent/telescope.lua
+++ lua/laurent/telescope.lua
@@ -1,21 +1,41 @@
 local M = {
     "nvim-telescope/telescope.nvim",
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
         defaults = {
-        -- Default configuration for telescope goes here:
-        -- config_key = value,
-        mappings = {
-            i = {
-                -- map actions.which_key to <C-h> (default: <C-/>)
-                -- actions.which_key shows the mappings for your picker,
-                -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-                -- ["<C-h>"] = "which_key"
-            }
-        }
+            -- Default configuration for telescope goes here:
+            -- config_key = value,
+            mappings = {
+                i = {
+                    -- map actions.which_key to <C-h> (default: <C-/>)
+                    -- actions.which_key shows the mappings for your picker,
+                    -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+                    -- ["<C-h>"] = "which_key"
+                },
+            },
         },
         pickers = {
+            find_files = {
+                hidden = true,
+                no_ignore = true,
+                file_ignore_patterns = {
+                    "venv/.*",
+                    ".venv/.*",
+                    "node_modules/.*",
+                    -- Add any other patterns you want to ignore
+                },
+            },
+            live_grep = {
+                hidden = true,
+                no_ignore = true,
+                file_ignore_patterns = {
+                    "venv/.*",
+                    ".venv/.*",
+                    "node_modules/.*",
+                    -- Add any other patterns you want to ignore
+                },
+            },
             -- Default configuration for builtin pickers goes here:
             -- picker_name = {
             --   picker_config_key = value,
@@ -30,8 +50,8 @@ local M = {
             --   extension_config_key = value,
             -- }
             -- please take a look at the readme of the extension you want to configure
-        }
-    }
+        },
+    },
 }
 
 return M
